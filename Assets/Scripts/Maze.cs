41d717a471863de8305d581dd24dc9c83a869478
Jeffrey Weng <46985822+jwengbing@users.noreply.github.com>
2020-07-25 13:25:39 -0400

    maZE Update

---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    public int width;
+
+    public int height;
+
+    public Tile[] tiles;
+
+    public List<Edge> edges;
+
+    public int EdgeIndex = 0;
+
+
+
+    public void Start()
+    {
+        tiles = new Tile[width * height];
+
+        for (int i = 0; i < width * height; i++)
+        {
+            tiles[i] = new Tile();
+        }
+
+        edges = new List<Edge>();
+
+
+
+        spawnLeftRightBoundaries();
+        spawnUpDownBoundaries();
+        spawnInnerEdgesLeftRight();
+        spawnInnerEdgesUpDown();
+
+
+        StartCoroutine(removeEdgeCoroutine());
+    }
+
+    public void spawnLeftRightBoundaries()
+    {
+        for (int z = 1; z <= height; z++)
+        {
+            for (int x = 0; x <= width; x++)
+            {
+                if (x == 0 || x == width)
+                {
+                    
+                    Instantiate(prefab, new Vector3((x * 5), 0, (z * 5)), Quaternion.Euler(0, 90, 0));
+                }
+
+            }
+        }
+
+    }
+
+    public void spawnUpDownBoundaries()
+    {
+        for (int z = 0; z <= height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                if (z == 0 || z == height)
+                {
+                    Instantiate(prefab, new Vector3((x * 5) + 2.5f, 0, (z * 5) + 2.5f), Quaternion.Euler(0, 0, 0));
+                }
+
+            }
+        }
+
+    }
+
+    public void spawnInnerEdgesLeftRight()
+    {
+        EdgeIndex = 0;
+
+        for (int z = 1; z <= height; z++)
+        {
+            for (int x = 1; x < width; x++)
+            {
+                GameObject go = (GameObject)Instantiate(prefab, new Vector3((x * 5), 0, (z * 5)), Quaternion.Euler(0, 90, 0));
+
+                Edge edge = go.AddComponent<Edge>() as Edge;
+
+
+
+                go.GetComponent<Edge>().tiles = new Tile[2];
+                go.GetComponent<Edge>().tiles[0] = tiles[EdgeIndex];
+                go.GetComponent<Edge>().tiles[1] = tiles[EdgeIndex + 1];
+
+
+
+                edges.Add(go.GetComponent<Edge>());
+
+                EdgeIndex++;
+
+            }
+            EdgeIndex++;
+        }
+    }
+
+    public void spawnInnerEdgesUpDown()
+    {
+        EdgeIndex = 0;
+
+        for (int z = 1; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                GameObject go = (GameObject)Instantiate(prefab, new Vector3((x * 5) + 2.5f, 0, (z * 5) + 2.5f), Quaternion.Euler(0, 0, 0));
+
+                Edge edge = go.AddComponent<Edge>() as Edge;
+
+                go.GetComponent<Edge>().tiles = new Tile[2];
+                go.GetComponent<Edge>().tiles[0] = tiles[EdgeIndex];
+                go.GetComponent<Edge>().tiles[1] = tiles[EdgeIndex + 1];
+
+                edges.Add(go.GetComponent<Edge>());
+
+                EdgeIndex++;
+
+            }
+
+        }
+    }
+
+
+    public void removeEdges()
+    { 
+
+        int randInt = Random.Range(0, edges.Count);
+
+        Edge randomEdge = edges[randInt];
+
+
+        edges.RemoveAt(randInt);
+
+        if (Tile.getHighestParent(randomEdge.tiles[0]) == Tile.getHighestParent(randomEdge.tiles[1]))
+        {
+            if (Tile.getHighestParent(randomEdge.tiles[0]) == null && Tile.getHighestParent(randomEdge.tiles[1]) == null)
+            {
+                randomEdge.tiles[0].parent = randomEdge.tiles[1];
+
+                randomEdge.disableEdge();
+        
+            }
+        }
+
+        else
+        {
+            if (Tile.getHighestParent(randomEdge.tiles[0]) == null && Tile.getHighestParent(randomEdge.tiles[1]) == null)
+            {
+                Tile.getHighestParent(randomEdge.tiles[0]).parent = randomEdge.tiles[1];
+                randomEdge.disableEdge();
+            }
+
+            else
+            {
+                Tile.getHighestParent(randomEdge.tiles[1]).parent = randomEdge.tiles[0];
+                randomEdge.disableEdge();
+            }
+        }
+
+
+
+
+
+    }
+
+    public IEnumerator removeEdgeCoroutine()
+    {
+        int loopNum = edges.Count;
+
+        for (int i = 0; i < loopNum; i++)
+        {
+            removeEdges();
+            yield return new WaitForSeconds(.2f);
+        }
+    }
+
+}
+
+
+
+
+[System.Serializable]
+public class Tile
+{
+
+    public Tile parent;
+
+    public static Tile getHighestParent(Tile tile)
+    {
+
+
+
+        if (tile.parent == null)
+        {
+            return tile;
+        }
+
+        else
+        {
+            return getHighestParent(tile.parent);
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/MazeCreation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class MazeCreation : MonoBehaviour
+{
+    public GameObject prefab;
+    public Vector3 pos; 
+    public int len;
+    public int x;
+    public int y;
+      // Start is called before the first frame update
+    void Start()
+    {
+        createGrid();
+    }
+
+    void createGrid()
+    {
+        pos = new Vector3( (-x / 2) + len / 2, 0.0f, (-y/ 2) + len / 2);
+        Vector3 newpos = pos;
+        GameObject temp; 
+        for(int i = 0; i< y; i++)
+        {
+            for(int j = 0; j<= x; j++)
+            {
+                pos = new Vector3(pos.x + (len * j) - len/2 , 0.0f, pos.z + len* i - len/2);
+                Instantiate(prefab, newpos, Quaternion.identity);
+            }
+        }
+
+        for (int i = 0; i <= y; i++)
+        {
+            for (int j = 0; j < x; j++)
+            {
+                pos = new Vector3(pos.x + len * j - len/2, 0.0f, pos.z + len * i - len/2);
+                temp = Instantiate(prefab, newpos, Quaternion.Euler(0.0f, 90.0f, 0.0f)) as GameObject;
+            }
+        }
+    }
+}
