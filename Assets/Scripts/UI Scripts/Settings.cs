638acd1918accaf95055b2e5d467764f4d520eb8
Eric Tong <eric.tong105@gmail.com>
2020-07-25 16:00:59 -0400

    #14

--- Assets/Scripts/UI Scripts/Settings.cs
+++ Assets/Scripts/UI Scripts/Settings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class Settings : MonoBehaviour
 {
@@ -9,23 +10,51 @@ public class Settings : MonoBehaviour
     public string keyCode;
 
     public Canvas canvas;
-    GameObject hud;
+    public GameObject GUI;
+    bool onDisplay = false;
+    Button button;
+
     // Start is called before the first frame update
     void Start()
     {
-        hud.SetActive(false);
+        GUI.SetActive(false);
+        button = this.GetComponent<Button>();
+        // button.interactable = true;
+        // button.enabled = false;
+        button.onClick.AddListener(OnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(keyCode)) {
-            onClick();
+            OnClick();
+        }
+        
+    }
+
+    void OnClick()
+    {
+        if (!onDisplay)
+        {
+            openGUI();
+        }
+        else
+        {
+            closeGUI();
         }
     }
 
-    void onClick() 
+    void openGUI() 
     {
-        hud.SetActive(true);
+        onDisplay = true;
+        GUI.SetActive(true);
+        
+    }
+
+    void closeGUI()
+    {
+        onDisplay = false;
+        GUI.SetActive(false);
     }
 }
