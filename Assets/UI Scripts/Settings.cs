26c47857597b983e4f7317004765e67d03b12cd2
Eric Tong <eric.tong105@gmail.com>
2020-07-25 13:46:30 -0400

    #14

---
+++ Assets/UI Scripts/Settings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    [Header(Settings)]
+    public Keycode key;
+
+    public Canvas canvas;
+    object hud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hud = canvas.GetComponent(SettingsUI);
+        hud.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.getKeyCode(key)) {
+            onClick();
+        }
+    }
+
+    void onClick() 
+    {
+        hud.SetActive(true);
+    }
+}
