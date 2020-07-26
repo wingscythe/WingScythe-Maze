90c449fb15275c5861399d1101f6e7c39e55e3a6
AndyZheng430 <zhenga6@bxscience.edu>
2020-07-25 23:21:10 -0400

    set up strawberry spawn

---
+++ Assets/Scripts/SpawnObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject Object;
+    public Vector3 ObjectLocation;
+    public Vector3 ObjectRotation;
+    private void OnTriggerStay(Collider other) {
+        // Debug.Log("enter");
+        if (Input.GetKeyDown(KeyCode.Q) && other.CompareTag("Player")) {
+            GameObject item = Instantiate(Object, Vector3.zero, Quaternion.Euler(0, 0, 0)) as GameObject;
+            // GameObject item = Instantiate(Object, ObjectLocation, ObjectRotation) as GameObject;            
+            Transform destination = other.transform.Find("gnome_model").Find("lower_body").Find("upper_body").Find("upper_arm.R").Find("lower_arm.R").Find("hand.R");
+            if (destination) {
+                Debug.Log("destination found");
+            }else {
+                Debug.Log("not found");
+            }
+            item.transform.parent = destination.transform;
+            item.transform.localPosition = ObjectLocation;
+            item.transform.localEulerAngles = ObjectRotation;
+            // Debug.Log(destination);
+            // Debug.Log("Object spawned");
+        }
+    }
+}
