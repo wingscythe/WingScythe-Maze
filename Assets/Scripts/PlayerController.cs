c670a5e883f51a807a8cb9f8a06db0cc7fcd9310
Jeffrey Weng <46985822+jwengbing@users.noreply.github.com>
2020-07-26 01:35:39 -0400

    maze

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -27,7 +27,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = moveSpeed;
+        speed = moveSpeed;   
         anims = gameObject.GetComponentInChildren<Animator>();
     }
     
