a28c90dc1d13314aa80c90e1bb18bbe7a13e8033
Jeffrey Weng <46985822+jwengbing@users.noreply.github.com>
2020-07-26 01:42:33 -0400

    test

--- Assets/Scripts/PUN2/PUN2_RoomController.cs
+++ Assets/Scripts/PUN2/PUN2_RoomController.cs
@@ -36,7 +36,7 @@ public class PUN2_RoomController : MonoBehaviourPunCallbacks {
         if(isMasterClient){
             //Control and sync maze spawn
             seed = Random.Range(int.MinValue, int.MaxValue);
-            photonView.RPC("setSeed", RpcTarget.All, seed);
+            photonView.RPC("setSeed", RpcTarget.AllBuffered, seed);
             currMaze = PhotonNetwork.Instantiate(mazePrefab.name, Vector3.zero , Quaternion.identity);
         }
 
